2e77d7d24e808de740e3a75fbea6e97dbd099c73
Manon Berne <mberne@student.42lyon.fr>
2022-08-04 16:09:05 +0200

    al done and fix except insert, erase and swap

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -5,6 +5,8 @@
 # include <string>
 # include <memory>
 # include "iterator.hpp"
+# include "enable_if.hpp"
+# include "is_integral.hpp"
 # include "equal.hpp"
 # include "lexicographical_compare.hpp"
 
@@ -44,11 +46,15 @@ namespace ft
 						_allocator.construct(_begin + i, val);
 				}
 				template <class InputIterator>
-					vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type()) : _allocator(alloc) {} // ?
+					vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type(), typename ft::enable_if<!ft::is_integral<InputIterator>::value, InputIterator>::type = NULL) : _allocator(alloc), _size(last - first), _capacity(_size) {
+						_begin = _allocator.allocate(_size);
+						for (size_type i = 0; i < _size; ++i)
+							_allocator.construct(_begin + i, first[i]);
+					}
 				vector(const vector &x) {
 					_allocator = x._allocator;
 					_size = x._size;
-					_capacity = x.capacity;
+					_capacity = x._capacity;
 					_begin = _allocator.allocate(_capacity);
 					for (size_type i = 0; i < _size; ++i)
 						_allocator.construct(_begin + i, x[i]);
@@ -59,7 +65,7 @@ namespace ft
 				~vector() {
 					for (size_type i = 0; i < _size; ++i)
 						_allocator.destroy(_begin + i);
-					_allocator.deallocate(_begin, _capacity);
+					// _allocator.deallocate(_begin, _capacity);
 				}
 
 	// Operator =
@@ -92,6 +98,8 @@ namespace ft
 				size_type max_size() const									{ return _allocator.max_size(); }
 				void resize (size_type n, value_type val = value_type())	{
 					value_type	value = 0;
+					if (val)
+						value = val;
 					if (n < _size)
 					{
 						for (size_type i = 0; i < _size - n; ++i)
@@ -101,10 +109,10 @@ namespace ft
 					}
 					else if (n > _size)
 					{
-						_begin = _allocator.allocate(n - _size, _begin);
-						if (val)
-							for (size_type i = 0; i < n - _size; ++i)
-								_allocator.construct(_begin + _size + i, val);
+						if (n > _capacity)
+							_reallocate_with_save(n);
+						for (size_type i = 0; i < _size - n; ++i)
+							_allocator.construct(_begin + _size + i, value);
 						_size = n;
 					}
 				}
@@ -116,16 +124,7 @@ namespace ft
 						if (n > max_size)
 							throw std::length_error("you tried to exceed maximum supported size");
 						else
-						{
-							pointer tmp = _allocator.allocate(n);
-							for (size_type i = 0; i < _size; ++i)
-								_allocator.construct(tmp + i, _begin[i]);
-							for (size_type i = 0; i < _size; ++i)
-								_allocator.destroy(_begin + i);
-							_allocator.deallocate(_begin, _capacity);
-							_begin = tmp;
-							_capacity = n;
-						}
+							_reallocate_with_save(n)
 					}
 				}
 
@@ -135,12 +134,12 @@ namespace ft
 				const_reference operator[] (size_type n) const	{ return _begin + n; }
 				reference at (size_type n)						{
 					if (n >= _size)
-						throw std::out_of_range("vector");
+						throw std::out_of_range("you tried to exceed memory not allocated");
 					return _begin + n;
 				}
 				const_reference at (size_type n) const			{
 					if (n >= _size)
-						throw std::out_of_range("vector");
+						throw std::out_of_range("you tried to exceed memory not allocated");
 					return _begin + n;
 				}
 				reference front()								{ return *_begin; }
@@ -154,9 +153,11 @@ namespace ft
 					void assign (InputIterator first, InputIterator last) {
 						for (size_type i = 0; i < _size; ++i)
 							_allocator.destroy(_begin + i);
-						if (last - first > _size)
+						if (last - first > _capacity)
 							_reallocate(last - first);
-						// remplir le vecteur avec les iterateurs
+						_size = last - first;
+						for (size_type i = 0; i < _size; ++i)
+							_allocator.construct(_begin + i, first[i])
 					}
 				void assign (size_type n, const value_type& val) {
 					for (size_type i = 0; i < _size; ++i)
@@ -168,22 +169,21 @@ namespace ft
 				}
 				void push_back (const value_type& val) {
 					if (_size == _capacity)
-					{
-						// reallouer en gardant les valeurs
-					}
+						_reallocate_with_save(_size * 2);
 					_allocator.construct(_begin + _size, val);
+					++_size;
 				}
 				void pop_back() {
 					_allocator.destroy(_begin + _size);
 					--_size;
 				}
-				iterator insert (iterator position, const value_type& val);	//
-				void insert (iterator position, size_type n, const value_type& val); //
-				template <class InputIterator>
-					void insert (iterator position, InputIterator first, InputIterator last); //
-				iterator erase (iterator position); //
-				iterator erase (iterator first, iterator last); //
-				void swap (vector& x); //
+				// iterator insert (iterator position, const value_type& val) {};
+				// void insert (iterator position, size_type n, const value_type& val);
+				// template <class InputIterator>
+				// 	void insert (iterator position, InputIterator first, InputIterator last);
+				// iterator erase (iterator position);
+				// iterator erase (iterator first, iterator last);
+				// void swap (vector& x);
 				void clear() {
 					for (size_type i = 0; i < _size; ++i)
 						_allocator.destroy(_begin + i);
@@ -197,14 +197,20 @@ namespace ft
 			protected :
 
 				void	_reallocate(size_type n) {
-					_allocator.deallocate(_begin, _size);
-					_size = n;
-					_begin = _allocator.allocate(_size);
-					_capacity = _size;
+					_allocator.deallocate(_begin, _capacity);
+					_capacity = n;
+					_begin = _allocator.allocate(_capacity);
 				}
 
 				void	_reallocate_with_save(size_type n) {
-					//
+					pointer tmp = _allocator.allocate(n);
+					for (size_type i = 0; i < _size; ++i)
+						_allocator.construct(tmp + i, _begin[i]);
+					for (size_type i = 0; i < _size; ++i)
+						_allocator.destroy(_begin + i);
+					_allocator.deallocate(_begin, _capacity);
+					_begin = tmp;
+					_capacity = n;
 				}
 
 		};
